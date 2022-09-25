480a357a1b177191fe54156128cdc101a8b862a7
rniochy <lniochy@gmail.com>
2022-09-25 18:06:41 +0100

    end  Regular expression Basics

---
+++ 02-quantfiers/index.js
@@ -0,0 +1,22 @@
+//  Regular expressions Quantifiers
+
+/**
+ * We use Quantifers to quantify how many  matches we have in our pattern   
+ * 
+ *  * - Zero ou more matches
+ *  + - One ou more matches
+ *  ? - Zero ou one
+ *  {} - is to match the amount defined as:
+ *  {2} - exactly 2 
+ *  {2, 5} - Between  2 an 5
+ *  {2, } -2 or more 
+ * */
+  
+   // let pattern = new RegExp("")
+   const zeroOrMore = /g*/.test("RRRRRRRRRRRRR");
+   const zeroOrOne = /R?/.test("");
+   const oneOrMore = /R+/.test("RR")
+   const exactlySix = /R{2}/.test("RRRRRRRR");
+   const betweenThreeAndSix = /R{2,6}/.test("RRR");
+   const SixOrMore = /R{6,}/.test("RRRRRRRRRRRRRRRRR");
+    console.log(zeroOrMore, oneOrMore, exactlySix, betweenThreeAndSix,SixOrMore);
