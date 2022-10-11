839ee3e0fad1e90545a868d49103f4905dda3789
rniochy <lniochy@gmail.com>
2022-10-11 18:16:12 +0100

    'ignore Case' flags

--- 04-flags/index.js
+++ 04-flags/index.js
@@ -7,7 +7,8 @@
 /**
  *     g - Global. Finds all matches instead of stopping after the first.
  *     i - Ignore case. /[a-z]/i is equivalent to /[a-zA-Z]/.
- *     m - Multiline. ^ and $ match the beginning and end of each line respectively treating \n and \r as delimiters instead
+ *     m - Multiline.
+ *     ^ and $ match the beginning and end of each line respectively treating \n and \r as delimiters instead
     of simply the beginning and end of the entire string.
       y - Finds all consecutive/adjacent matches.
 */
@@ -19,4 +20,6 @@
         }
 
     const ignoreCase = /(RodriGo)/i.test("rodrigo");
-    console.log(ignoreCase);
+
+    const multLine = /(Lima)$/.test("Lioma\nLikma\n\r\nLima");
+    console.log(ignoreCase, multLine);
