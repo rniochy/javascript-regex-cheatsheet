ba031d19219ca176e2381568614651be586895cb
rniochy <lniochy@gmail.com>
2022-10-11 17:22:06 +0100

    RegEx groups

--- 03-groups/index.js
+++ 03-groups/index.js
@@ -3,7 +3,8 @@
 /**
      *  We use the Group in RegEx to group some patterns 
      * (...) - Capturing Group
-     * (?:....) - Non-Capturing Group
+     * (?:....) - Non-Capturing Group, Using the form (?:pattern), these work in a similar way to capture groups, except they do not store the contents
+                of the group after the match.
      * \Y Match the Y'th Captured Group ???
  */
 
