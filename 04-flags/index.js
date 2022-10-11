75909acc4b1b7d39397bc236065b7bd51bdd1ee1
rniochy <lniochy@gmail.com>
2022-10-11 17:35:02 +0100

    flags

--- 04-flags/index.js
+++ 04-flags/index.js
@@ -9,7 +9,5 @@
  *     i - Ignore case. /[a-z]/i is equivalent to /[a-zA-Z]/.
  *     m - Multiline. ^ and $ match the beginning and end of each line respectively treating \n and \r as delimiters instead
     of simply the beginning and end of the entire string.
-       u - Unicode. If this flag is not supported you must match specific Unicode characters with \uXXXX where XXXX is the
-character's value in hexadecimal.
       y - Finds all consecutive/adjacent matches.
 */
