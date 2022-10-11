66b4804d3320e2b79e155f567d7a19d717985a67
rniochy <lniochy@gmail.com>
2022-10-11 17:27:40 +0100

    flags

---
+++ 04-flags/index.js
@@ -0,0 +1,10 @@
+/**
+ * There are several flags you can specify to alter the RegEx behaviour. Flags may be appended to the end of a regex
+    literal, such as specifying gi in /test/gi, or they may be specified as the second argument to the RegExp
+    constructor, as in new RegExp('test', 'gi')
+*/
+
+/**
+ * g - Global. Finds all matches instead of stopping after the first.
+ * i - Ignore case. /[a-z]/i is equivalent to /[a-zA-Z]/.
+*/
