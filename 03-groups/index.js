4638a0223a0229a36e80461f080ab07265efbc96
rniochy <lniochy@gmail.com>
2022-10-11 17:10:48 +0100

    groups Non-Capturing Group

--- 03-groups/index.js
+++ 03-groups/index.js
@@ -4,8 +4,10 @@
      *  We use the Group in RegEx to group some patterns 
      * (...) - Capturing Group
      * (?:....) - Non-Capturing Group
-     * \Y Match the Y'th Captured Group
+     * \Y Match the Y'th Captured Group ???
  */
 
-    const captureGroup = /(?:(Ro)(\w+)(o)$)/m.exec("Lima Rodrigo"); ///(?:(Ro)drig(o)$)/m.test("Lima Rodrigo");
-    console.log(captureGroup);
+    const captureGroup = /((Ro)(\w+)(o)$)/.exec("Lima Rodrigo"); // Capture the group where match
+
+    const noncaptureGroup = /(?:(Ro)drig(o)$)/.exec("Lima Rodrigo"); 
+    console.log(captureGroup, noncaptureGroup);
