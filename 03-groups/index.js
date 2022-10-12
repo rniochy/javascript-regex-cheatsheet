99447fd40158df5344a30183c06e26bf607ee887
rniochy <lniochy@gmail.com>
2022-10-12 16:20:25 +0100

    changes in groups folder

--- 03-groups/index.js
+++ 03-groups/index.js
@@ -42,3 +42,14 @@ If we put a quantifier after the parentheses, it applies to the parentheses as a
     const str = "2019-04-30";
 
     const groups = str.match(dateRegexp).groups;
+
+
+//     let regexp = /^[0-9a-f]{2}(:[0-9a-f]{2}){5}$/i;
+
+// alert( regexp.test('01:32:54:67:89:AB') ); // true
+
+// alert( regexp.test('0132546789AB') ); // false (no colons)
+
+// alert( regexp.test('01:32:54:67:89') ); // false (5 numbers, need 6)
+
+// alert( regexp.test('01:32:54:67:89:ZZ') ) // false (ZZ in the end)
