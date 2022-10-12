e216723b1126b215f9895f772262e0d15f336271
rniochy <lniochy@gmail.com>
2022-10-12 15:51:40 +0100

    changes in groups folder

--- 03-groups/index.js
+++ 03-groups/index.js
@@ -18,15 +18,27 @@ If we put a quantifier after the parentheses, it applies to the parentheses as a
 
     const captureGroup = /((Ro)(\w+)(o)$)/.exec("Lima Rodrigo"); // Capture the group where match
 
-    const noncaptureGroup = /(?:(Ro)drig(o)$)/.exec("Lima Rodrigo"); 
+    const noncaptureGroup = /((?:Ro)drig(o)$)/.exec("Lima Rodrigo"); 
     console.log(captureGroup, noncaptureGroup);
+
+
+    const str1 = "Gogogo John!";
+
+    // ?: excludes 'go' from capturing
+    const regexp = /(?:go)+ (\w+)/i;
+
+    const result = str1.match(regexp);
+
+    console.log( result[0] ); // Gogogo John (full match)
+    console.log( result[1] ); // John
+    console.log( result.length ); // 2 (no more items in the array
 
     // Named Groups
     /**
           Remembering groups by their numbers is hard. For simple patterns it’s doable, but for more complex ones counting parentheses is inconvenient. We have a much better option: give names to parentheses.
         That’s done by putting ?<name> immediately after the opening paren.
      */
-    let dateRegexp = /(?<year>[0-9]{4})-(?<month>[0-9]{2})-(?<day>[0-9]{2})/;
-    let str = "2019-04-30";
+    const dateRegexp = /(?<year>[0-9]{4})-(?<month>[0-9]{2})-(?<day>[0-9]{2})/;
+    const str = "2019-04-30";
 
-    let groups = str.match(dateRegexp).groups;
+    const groups = str.match(dateRegexp).groups;
