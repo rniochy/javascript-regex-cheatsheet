52561edfa73838a47255025bbb5a3445ff604bc4
rniochy <lniochy@gmail.com>
2022-10-15 13:35:30 +0100

    added new  features

--- 08-assertion/index.js
+++ 08-assertion/index.js
@@ -6,3 +6,10 @@
  * (?= ...) - Positive lookhead
  * (?! ...) - Negative lookhead
 */
+
+const startOfString = /^J/.exec("Joh Doe");
+const endOfString = /e$/.exec("Joh Doe");
+const wordBoundary = /\b/.exec("John Doe");
+console.table(startOfString);
+console.table(endOfString);
+console.table(wordBoundary);
