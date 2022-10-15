2747b85efbb988f69f5c895f5d7ed652f8f5ca42
rniochy <lniochy@gmail.com>
2022-10-15 12:59:46 +0100

    added new  features

---
+++ 06-caracter-class/index.js
@@ -0,0 +1,15 @@
+/**
+ *  Character Classes are used to match regex based on them
+*/
+
+/**
+ *  [ab-d]
+ *  [^ab-d]
+ *  [\b]
+ *  \d  - One digit
+ *  \D  - One non-digit
+ *  \s  - One whitespace
+ *  \S  - One non-white espace
+ *  \w  - One word character
+ *  \W  - One  non-word character
+ */
