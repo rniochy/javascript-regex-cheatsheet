2647428c0a8347d37dd4a8c23846b7a5ba9b3c14
rniochy <lniochy@gmail.com>
2022-10-28 17:12:08 +0100

    finished word boundary

--- 08-assertion/index.js
+++ 08-assertion/index.js
@@ -3,13 +3,25 @@
  *  $ - end of string
  * \b - word boundary
  *  \B - Non-word boundary
- * (?= ...) - Positive lookhead
+ * (?= ...) - Positive lookhead - The syntax is: X(?=Y), it means "look for X, but match only if followed by Y". There may be any pattern instead of X and Y.
  * (?! ...) - Negative lookhead
 */
 
 const startOfString = /^J/.exec("Joh Doe");
 const endOfString = /e$/.exec("Joh Doe");
-const wordBoundary = /\b/.exec("John Doe");
+
+const wordBoundary = /\bJohn\b/.exec("John Doe");
+const NonWordBoundary = /\BJohn\B/.exec("John Doe");
+/**
+ *  WORD BOUNDARY
+ *  We can use \b not only with words, but with digits as well. 
+ * */ 
+const wordBoundaryWithDigits = /\b\d\d\b/g.exec("1 23 456 78");
+
+
+
 console.table(startOfString);
 console.table(endOfString);
 console.table(wordBoundary);
+console.table(wordBoundaryWithDigits);
+console.table(NonWordBoundary);
