fcd9346156e8e0be1fbf21374ec9186a02cc313d
rniochy <lniochy@gmail.com>
2022-10-30 16:59:14 +0100

    Finished to implement 05-replacement

--- 05-replacement/index.js
+++ 05-replacement/index.js
@@ -14,15 +14,40 @@
 
 
 /**
- * $number - 	Includes the last substring matched by the capturing group that is identified by number.
+ * $Y - Insert Y'th captures group - Includes the last substring matched by the capturing group that is identified by number.
  * ${name} - Includes the last substring matched by the named group that is designated by (?<name> ) in the replacement string.
  * $$ - Includes a single "$" literal in the replacement string.
  * $& - Includes a copy of the entire match in the replacement string.
  * $` - Includes all the text of the input string before the match in the replacement string.
  * $' - Includes all the text of the input string after the match in the replacement string.
- * $Y - Insert Y'th captures group
 */
 
 let str = "foo";
-const regex = /(f)/
-console.log(str.replace(regex, "$1go"));
+const regexNumberGroup = /(f)/;
+str = str.replace(regexNumberGroup, "$1 go")
+console.log(str);
+
+let str1 = "foo";
+const regexNamedGroup = /(?<group>f)/;
+str1 = str1.replace(regexNamedGroup, "$<group> ")
+console.log(str1);
+
+let str2 = "foo";
+const regexSingleLiteral$ = /(f)/;
+str2 = str2.replace(regexSingleLiteral$, "$$")
+console.log(str2);
+
+let str3 = "foo";
+const regexIntereMatch = /(f)/;
+str3 = str3.replace(regexIntereMatch, "$& ")
+console.log(str3);
+
+let str4 = "foo";
+const regexAlltextBeforeMatch = /(f)/;
+str4 = str4.replace(regexAlltextBeforeMatch, "$` ")
+console.log(str4);
+
+let str5 = "foo";
+const regexAlltextAfterMatch = /(f)/;
+str5 = str5.replace(regexAlltextAfterMatch, "$' ")
+console.log(str5);
